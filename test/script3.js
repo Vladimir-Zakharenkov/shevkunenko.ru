fbe3f61836579c1d51f41c59511638c48c225ac3
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-13 20:21:06 +0300

    13-11-2022-20-21

---
+++ test/script3.js
@@ -0,0 +1,42 @@
+var zmn = jQuery.noConflict()
+
+zmn(document).ready(function()
+	{
+		zmn(".guess_box").click(CheckForCode);
+		function GetRandom (num)
+			{
+				var my_num = Math.floor(Math.random()*num);
+				return my_num;
+			}
+		var hideCode = function ()
+			{
+				var numRand = GetRandom(4);
+				zmn(".guess_box").each(function(index, value)
+										{
+											if(numRand == index)
+												{
+													zmn(this).append("<span id='has_discount'></span>");
+													return false;
+												}
+										});
+			}
+		hideCode();
+		function CheckForCode()
+			{
+				var discount;
+				if(zmn.contains(this, document.getElementById("has_discount")))
+					{
+						var my_num = GetRandom(5);
+						discount = "<p>Your discount is " + my_num + "%</p>";
+					}
+					else
+						{
+							discount = "<p>Sory, no discount this time!</p>";
+						}
+				zmn(this).append(discount);
+				zmn(".guess_box").each(function()
+									{
+										zmn(this).unbind('click');
+									});
+			}
+	}); //End document.raedy
